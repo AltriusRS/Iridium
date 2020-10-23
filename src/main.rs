b1b03d7f9a191a49e6f155378ea536a5c11219bd
fatalcenturion <tom.b.2k2@gmail.com>
2020-10-23 18:54:07 +0100

    z

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+const HEADERS: &str = "<link rel=\"stylesheet\" href=\"/path/to/styles/default.css\">\n<script src=\"/path/to/highlight.min.js\"></script>\n<script>hljs.initHighlightingOnLoad();</script>";
+
 fn main() {
     println!("Hello, world!");
 }
