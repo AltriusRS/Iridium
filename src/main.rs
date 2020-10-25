a58f53693243df70b3ec8c99acaf264a24038eed
fatalcenturion <tom.b.2k2@gmail.com>
2020-10-25 01:14:39 +0100

    its getting closer to production ready, got some stylesheet tweaking to do before shipping 0.0.5

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use regex::Regex;
+use scraper::{Html, Selector};
 use clap::{Arg, App};
-use comrak::{markdown_to_html, ComrakOptions, ComrakExtensionOptions};
+use comrak::{markdown_to_html, ComrakOptions, ComrakExtensionOptions, ComrakParseOptions, ComrakRenderOptions};
 use std::fs::*;
 use std::io::{Read, Write};
 use std::path::PathBuf;
+use std::ops::{Range, RangeInclusive};
 
 const HEADERS: &str = "<link rel=\"stylesheet\" href=\"http://github.com/yrgoldteeth/darkdowncss/raw/master/darkdown.css\" />\n<link rel=\"stylesheet\" href=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/default.min.css\">\n<script src=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/highlight.min.js\"></script>\n<script>hljs.initHighlightingOnLoad();</script>";
 
@@ -25,8 +28,15 @@ fn main() {
             .help("Sets the location to write to. (Must be a directory)")
             .takes_value(true)
             .required(true))
+        .arg(Arg::with_name("watermark")
+            .long("no-water-mark")
+            .help("Removes the watermark")
+            .takes_value(false))
         .get_matches();
-
+    let mut wm = true;
+    if matches.is_present("watermark") {
+        wm = false;
+    }
     if matches.is_present("in") && matches.is_present("out") {
         let input = matches.value_of("in").unwrap();
         let output = matches.value_of("out").unwrap();
@@ -34,19 +44,34 @@ fn main() {
         if metadata.is_ok() {
             let md = metadata.unwrap();
             if md.is_dir() {
+                println!("Discovering Files...");
                 let paths = read_directory(input);
-                println!("Found: {} files to process", paths.len());
+                let tot = paths.len();
+                println!("Discovered {} Files", tot);
+                println!("Migrating incompatible files...");
                 let processes = handle_non_md(paths, input, output);
-                println!("Final Queue: {} files", processes.len());
+                let mut index = tot - processes.len();
+                let ptot = processes.len();
+                println!("Migrated {} Files", index);
+                let mut progress: f32 = ((100 / tot) * index) as f32;
+
+                for (source, destination) in processes {
+                    print!("\rCompiling: {}%", progress);
+                    read_file(source, destination, wm);
+                    index += 1;
+                    progress = ((100 / tot) * index) as f32;
+                }
+                println!("\rCompiling: 100%\nCompiled {} Files", ptot);
             } else {
                 println!("")
                 // read_file();
             }
         } else {
-            println!("An error occurred: {:#?}", metadata.unwrap_err());
+            println!("An error occurred (step 1): {:#?}", metadata.unwrap_err());
             std::process::exit(1);
         }
     } else {}
+    println!("Compilation complete.")
 }
 
 fn read_directory(path: &str) -> Vec<PathBuf> {
@@ -68,23 +93,26 @@ fn read_directory(path: &str) -> Vec<PathBuf> {
     paths
 }
 
-fn handle_non_md(paths: Vec<PathBuf>, base: &str, out: &str) -> Vec<PathBuf> {
-    let mut processes = Vec::<PathBuf>::new();
+fn handle_non_md(paths: Vec<PathBuf>, base: &str, out: &str) -> Vec<(String, String)> {
+    let mut processes = Vec::<(String, String)>::new();
     for path in paths {
+        let mut root = String::from(base);
+        if !root.ends_with("/") {
+            root = format!("{}/", root)
+        }
+        let mut destination = String::from(out);
+        if !destination.ends_with("/") {
+            destination = format!("{}/", destination)
+        }
+
         let pathstr = path.to_str().unwrap();
+        let p2 = pathstr.clone();
+        let mut tiers = p2.split(root.as_str()).collect::<Vec<&str>>();
+        let final_path = format!("{}{}", destination, tiers.pop().unwrap());
+
         if pathstr.ends_with(".md") {
-            processes.push(path);
+            processes.push((pathstr.to_string(), final_path));
         } else {
-            let mut root = String::from(base);
-            if !root.ends_with("/") {
-                root = format!("{}/", root)
-            }
-            let mut destination = String::from(out);
-            if !destination.ends_with("/") {
-                destination = format!("{}/", destination)
-            }
-            let mut tiers = pathstr.split(root.as_str()).collect::<Vec<&str>>();
-            let final_path = format!("{}{}", destination, tiers.pop().unwrap());
             let mut meta = metadata(&destination);
             if meta.is_ok() {
                 make_file(final_path, pathstr.to_string());
@@ -101,31 +129,36 @@ fn handle_non_md(paths: Vec<PathBuf>, base: &str, out: &str) -> Vec<PathBuf> {
 }
 
 fn make_file(path: String, root: String) {
-    println!("{}\t{}", path, root);
     let meta = metadata(&path);
     let mut content = Vec::<u8>::new();
     let available = File::open(root);
+
     if available.is_ok() {
         available.unwrap().read_to_end(&mut content);
     } else {
-        println!("{}", available.unwrap_err().to_string());
+        println!("Error - File unavailable: {}", available.unwrap_err().to_string());
     }
+
 
     if meta.is_ok() {
         remove_file(&path);
         let mut file = File::create(&path).unwrap();
         file.write(content.as_mut());
     } else {
-        println!("{:#?}", meta);
         let err = meta.unwrap_err();
         if err.kind() == std::io::ErrorKind::NotFound {
+            let op = path.clone();
+            let mut entries = op.split("/").collect::<Vec<&str>>();
+            let _ = entries.pop();
+            let dir_path = entries.join("/");
+            create_dir_all(dir_path);
             let mut file = File::create(&path).unwrap();
             file.write(content.as_mut());
         }
     }
 }
 
-fn read_file(path: String, out_path: String) {
+fn read_file(path: String, out_path: String, wm: bool) {
     // Read the file
     let mut content_file = File::open(path).unwrap();
     let mut content = String::new();
@@ -136,13 +169,17 @@ fn read_file(path: String, out_path: String) {
     let mut res = File::open("./dark.css").unwrap();
     let read = res.read_to_string(&mut css).unwrap();
 
+    let mut watermark = String::from("<div style=\"text-align: center; padding: 1em; color: #aaa\"><h4>Powered by <a href=\"https://github.com/fatalcenturion/Iridium\">Iridium</a><h4></div>");
+    if !wm {
+        watermark = String::new();
+    }
     // Begin parsing MD to HTML
     let mut html = format!("<!DOCTYPE html>\n<html>\n<head>\n{}\n<style>{}</style>", HEADERS, css);
-    html = format!("{}\n</head>\n<body>\n<div class=\"container\">", html);
-    html = format!("{}\n{}\n</div>\n</body>\n<html>", html, markdown_to_html(&content.as_str(), &ComrakOptions {
+    html = format!("{}\n<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>\n</head>\n<body>\n<div class=\"container\">", html);
+    html = format!("{}\n{}\n</div>{}\n{}</body>\n<html>", html, markdown_to_html(&content.as_str(), &ComrakOptions {
         extension: ComrakExtensionOptions {
             strikethrough: true,
-            tagfilter: true,
+            tagfilter: false,
             table: true,
             autolink: true,
             tasklist: true,
@@ -151,27 +188,64 @@ fn read_file(path: String, out_path: String) {
             footnotes: true,
             description_lists: true,
         },
-        parse: Default::default(),
-        render: Default::default(),
-    }));
+        parse: ComrakParseOptions {
+            smart: false,
+            default_info_string: None,
+        },
+        render: ComrakRenderOptions {
+            hardbreaks: false,
+            github_pre_lang: false,
+            width: 0,
+            unsafe_: true,
+            escape: false,
+        },
+    }), "<script>$(document).ready(function(){$(\"a\").on(\"click\",function(o){if(\"\"!==this.hash){o.preventDefault();var t=this.hash;$(\"html, body\").animate({scrollTop:$(t).offset().top},800,function(){window.location.hash=t})}})});</script>", watermark);
 
+    let matcher = Regex::new(r"<\s*a[^>]*>").unwrap();
+    let htclone = html.clone();
+    let matches = matcher.find_iter(&htclone).collect::<Vec<regex::Match>>();
+    for token in matches {
+        let start = token.start();
+        let end = token.end();
+        let content = token.as_str();
+        let fragment = Html::parse_fragment(content);
+        let body = html.split(content).collect::<Vec<&str>>();
+        let slice = fragment.select(&Selector::parse(r#"a"#).unwrap()).next().unwrap();
+        let mut href: String = String::from(slice.value().attr("href").unwrap());
+        if href.starts_with("//") || href.starts_with("http") {
+            // ignore the link
+        } else {
+            if href.contains('#') {
+                let collection = href.split('#').collect::<Vec<&str>>();
+                let sref: String = collection.join(".html#");
+                href = sref;
+            } else {
+                href = format!("{}.html", href);
+            }
+            html = body.join(format!("<a href=\"{}\">", href).as_str())
+        }
+    }
     let mut out: File;
     let meta = metadata(&out_path);
     if meta.is_ok() {
         let trydel = remove_file(&out_path);
         if trydel.is_ok() {
-            out = File::create(&out_path).unwrap();
+            out = File::create(&out_path.replace(".md", ".html")).unwrap();
             write(html, &mut out)
         } else {
             println!("Failed to parse \"{}\".\nReason: {:#?}", &out_path, trydel.unwrap_err());
         }
     } else {
-        out = File::create(&out_path).unwrap();
+        let op = out_path.clone();
+        let mut entries = op.split("/").collect::<Vec<&str>>();
+        let _ = entries.pop();
+        let dir_path = entries.join("/");
+        create_dir_all(dir_path);
+        out = File::create(&out_path.replace(".md", ".html")).unwrap();
         write(html, &mut out)
     }
 }
 
 fn write(content: String, file: &mut File) {
     file.write(content.as_bytes());
-    println!("Done");
 }
